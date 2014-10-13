cf75a052f28c4721be6c793aa39ba593e3bd1388
Maksim Chemerisuk <chemerisuk@gmail.com>
2014-10-14 02:55:13 +0300

    DRY code, extract doGetOpen and doSetOpen

--- src/better-details-polyfill.js
+++ src/better-details-polyfill.js
@@ -7,51 +7,51 @@
 
     DOM.extend("details", condition, {
         constructor() {
-            var opened = this.get("open");
-            opened = opened === "" || opened === "open";
             // http://www.w3.org/html/wg/drafts/html/master/interactive-elements.html#the-details-element
-            this.set("role", "group").set("aria-expanded", opened);
+            this.set("role", "group");
             this.children("summary:first-child").forEach(this.doInitSummary);
-            this.doDefineProperty("open", opened);
+            this.doDefineProperty();
         },
         doInitSummary(summary) {
+            // http://www.w3.org/html/wg/drafts/html/master/interactive-elements.html#the-summary-element
             summary
                 .set("tabindex", 0) // make summary to be focusable
-                .set("role", "button") // http://www.w3.org/html/wg/drafts/html/master/interactive-elements.html#the-summary-element
+                .set("role", "button")
                 .on("click", ["currentTarget"], this.doToggleOpen)
                 .on("keydown", ["currentTarget", "which"], this.onKeyDown);
         },
-        doDefineProperty(propName, opened) {
-            var node = this[0];
+        doDefineProperty() {
+            var opened = this.get("open");
 
-            Object.defineProperty(node, propName, {
-                get() {
-                    return opened;
-                },
-                set(value) {
-                    opened = !!value;
-                    node.setAttribute("aria-expanded", opened);
-
-                    if (opened) {
-                        node.setAttribute(propName, "", 1);
-                    } else {
-                        node.removeAttribute(propName, 1);
-                    }
-                    /* istanbul ignore if */
-                    if (JSCRIPT_VERSION < 9) {
-                        // fix refresh issue in IE8
-                        node.className = node.className;
-                    }
-                }
+            Object.defineProperty(this[0], "open", {
+                get: this.doGetOpen,
+                set: this.doSetOpen
             });
+
+            // trigger initial state
+            this.set("open", opened === "" || opened === "open");
+        },
+        doGetOpen() {
+            var node = this[0];
+            var opened = String(node.getAttribute("open"));
+
+            return opened === "" || opened.toLowerCase() === "open";
+        },
+        doSetOpen(value) {
+            var node = this[0];
+            var propName = JSCRIPT_VERSION < 9 ? "OPEN" : "open";
+
+            node.setAttribute("aria-expanded", !!value);
+
+            if (value) {
+                node.setAttribute(propName, "", 1);
+            } else {
+                node.removeAttribute(propName, 1);
+            }
             /* istanbul ignore if */
             if (JSCRIPT_VERSION < 9) {
-                // trick to avoid infinite recursion in IE8
-                propName = propName.toUpperCase();
-
-                if (opened) {
-                    node.setAttribute(propName, opened, 1);
-                }
+                // fix refresh issue in IE8
+                node.className = node.className;
             }
         },
         doToggleOpen(summary) {
