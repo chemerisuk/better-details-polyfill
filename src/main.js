3e6619b55913d6d077c4400274db9c8739fd5b01
Maksim Chemerisuk <chemerisuk@gmail.com>
2014-12-06 15:14:53 +0300

    add ARIA attributes for all browsers

--- src/main.js
+++ src/main.js
@@ -1,32 +1,45 @@
 (function(DOM, VK_SPACE, VK_ENTER) {
     "use strict";
 
-    // invoke extension only if there is no native support
-    var open = DOM.create("details").get("open");
+    // add ARIA attributes for ALL browsers because current
+    // native implementaions have weak support:
+    // https://bugs.webkit.org/show_bug.cgi?id=131111
 
-    DOM.extend("details", typeof open !== "boolean", {
+    // invoke extension only if there is no native support
+    var hasNativeSupport = typeof DOM.create("details").get("open") === "boolean";
+
+    DOM.extend("details", {
         constructor() {
             // http://www.w3.org/html/wg/drafts/html/master/interactive-elements.html#the-details-element
             this.set("role", "group")
-                .on("toggle", ["stopPropagation"], (stop) => { stop() })
-                .define("open", this._getOpen, this._setOpen);
+                .on("toggle", ["stopPropagation"], this._changeOpen.bind(this));
 
-            var summaries = this.children("summary");
+            var firstSummary = this.children("summary")[0];
             // If there is no child summary element, the user agent
             // should provide its own legend (e.g. "Details")
-            this._initSummary(summaries[0] || DOM.create("summary>`Details`"));
-        },
-        _initSummary(summary) {
-            // make sure that the <summary> is the first child
-            if (this.child(0) !== summary) {
-                this.prepend(summary);
+            if (!firstSummary) firstSummary = DOM.create("summary>`Details`");
+            // make the first <summary> always to be the first child
+            if (this.child(0) !== firstSummary) {
+                this.prepend(firstSummary);
             }
 
-            // http://www.w3.org/html/wg/drafts/html/master/interactive-elements.html#the-summary-element
-            summary
-                .set({role: "button", tabindex: 0})
-                .on("keydown", ["which"], this._toggleOpen.bind(this))
-                .on("click", this._toggleOpen.bind(this));
+            firstSummary.set("role", "button");
+
+            if (!hasNativeSupport) {
+                this.define("open", this._getOpen, this._setOpen);
+                // http://www.w3.org/html/wg/drafts/html/master/interactive-elements.html#the-summary-element
+                firstSummary
+                    .set("tabindex", 0)
+                    .on("keydown", ["which"], this._toggleOpen.bind(this))
+                    .on("click", this._toggleOpen.bind(this));
+            }
+
+            this._changeOpen();
+        },
+        _changeOpen(stop) {
+            this.set("aria-expanded", this.get("open"));
+
+            if (stop) stop(); // toggle event should not bubble
         },
         _getOpen(attrValue) {
             attrValue = String(attrValue).toLowerCase();
@@ -38,10 +51,8 @@
 
             propValue = !!propValue;
 
-            this.set("aria-expanded", propValue);
-
             if (currentValue !== propValue) {
-                this.fire("toggle");
+                setTimeout(() => { this.fire("toggle") }, 0);
             }
 
             return propValue ? "" : null;
